cdedaec99b2e92f4c0b717c7f19ab636f38ffabf
Gosha Komkov <homosaipios@mail.ru>
2023-07-26 17:28:58 +0300

    третий шаг, добавил i18next

--- src/app.js
+++ src/app.js
@@ -1,5 +1,66 @@
-import validedForm from './validedForm';
+import onChange from 'on-change';
+import * as yup from 'yup';
+import i18next from 'i18next';
+import renderValid from './view';
+import resources from './locales/index';
 
 export default () => {
-  validedForm();
+  const state = {
+    formState: 'filling',
+    error: null,
+    feeds: [],
+    url: '',
+  };
+
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  });
+  const form = document.querySelector('.rss-form');
+  const watchedState = onChange(state, () => {
+    renderValid(watchedState);
+  });
+  const validateUrl = () => {
+    yup.setLocale({
+      string: {
+        url: () => i18nextInstance.t('errors.notUrl'),
+        required: () => i18nextInstance.t('errors.empty'),
+      },
+      mixed: {
+        notOneOf: () => i18nextInstance.t('errors.alreadyInList'),
+      },
+    });
+
+    const schema = yup.object().shape({
+      url: yup.string().url().required().notOneOf(watchedState.feeds),
+    });
+
+    return schema
+      .validate({ url: watchedState.url })
+      .then(() => {
+        watchedState.error = null;
+        watchedState.formState = 'sending';
+      })
+      .catch((err) => {
+        watchedState.formState = 'inValid';
+        watchedState.error = err.message;
+      });
+  };
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    watchedState.error = null;
+    watchedState.url = document.querySelector('#url-input').value;
+
+    validateUrl().then(() => {
+      if (!watchedState.error) {
+        watchedState.feeds.push(watchedState.url);
+        watchedState.formState = 'filling';
+        watchedState.url = '';
+      }
+    });
+  });
+
+  watchedState();
 };
---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    errors: {
+      notUrl: 'Ссылка должна быть валидным URL',
+      alreadyInList: 'RSS уже существует',
+      empty: 'Не должно быть пустым',
+    },
+    status: {
+      success: 'RSS успешно загружен',
+    },
+  },
+};
