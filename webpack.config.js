fcdadce035760bb64fce76d21a8a9c5bf559c280
Gosha Komkov <homosaipios@mail.ru>
2023-09-09 16:24:36 +0300

    поправили конфиг

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,25 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import TerserPlugin from 'terser-webpack-plugin';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const entryPath = path.resolve(dirname, 'src', 'index.js');
+const outputPath = path.resolve(dirname, 'public');
 
 export default {
   mode: process.env.NODE_ENV || 'development',
-  entry: path.resolve(process.cwd(), './src/index.js'),
+  entry: entryPath,
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(process.cwd(), './public/'),
+    path: outputPath,
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(process.cwd(), './index.html'),
+      template: path.resolve(dirname, 'index.html'),
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
